614b17093549f1f8d2ed20e10c183d7219d64039
Fabio <95591197>
2022-07-05 16:04:19 +0200

    set route to book spots in a trip

--- routers/plannedTripsRouter.js
+++ routers/plannedTripsRouter.js
@@ -13,4 +13,30 @@ router.get("/", authMiddleware, async (req, res, next) => {
   }
 });
 
+router.patch("/:id/inscription", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { numberOfKids } = req.body;
+    console.log("REQUEST PARAMS ID", id);
+    const plannedTrip = await PlannedTrips.findByPk(id);
+    if (!id) {
+      return res.status(404).send("Planned trip not found");
+    }
+    if (numberOfKids > plannedTrip.capacity) {
+      return res
+        .status(404)
+        .send(`There's only ${plannedTrip.capacity} spots left`);
+    }
+
+    await PlannedTrips.update(
+      { capacity: plannedTrip.capacity - numberOfKids },
+      { where: { id: plannedTrip.id } }
+    );
+
+    res.send({ message: `${numberOfKids} spots booked` });
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
